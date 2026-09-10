59f367b63d82a2e8a73957516ec4a45d9bf896a8
Muqtxdir <muqtxdir@gmail.com>
2026-09-10 18:35:26 +0530

    chore: use XDG_PATHs exported background

--- src/components/unity-lock-background.c
+++ src/components/unity-lock-background.c
@@ -2,24 +2,10 @@
  *
  * Copyright 2026 Muqtadir
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #include "unity-lock-background.h"
-
 
 GdkPaintable *
 unity_lock_background_get (void)
@@ -29,8 +15,8 @@ unity_lock_background_get (void)
 
   if (g_once_init_enter (&loaded))
     {
-      g_autofree gchar *path = g_build_filename (UNITY_LOCK_MIRROR_ROOT,
-                                                 g_get_user_name (),
+      g_autofree gchar *path = g_build_filename (g_get_user_cache_dir (),
+                                                 "unity-shell",
                                                  "background.png", NULL);
       g_autoptr (GError) error = NULL;
 

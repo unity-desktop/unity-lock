4b8ae9f43dbc0fc085f00cbac03077e6b7487832
Muqtxdir <muqtxdir@gmail.com>
2026-09-10 18:22:30 +0530

    chore: shorten license headers

--- src/components/unity-lock-font-face.c
+++ src/components/unity-lock-font-face.c
@@ -2,19 +2,6 @@
  *
  * Copyright 2026 Muqtadir
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -23,7 +10,7 @@
 #include <glib/gstdio.h>
 #include <pango/pangocairo.h>
 
-#define FONT_RESOURCE_PREFIX "/org/unity/Lock/fonts/"
+#define FONT_RESOURCE_PREFIX "/org/unity/lock/fonts/"
 #define N_STYLES (UNITY_LOCK_FONT_FACE_STYLE_CLIMATE_CRISIS + 1)
 
 G_DEFINE_ENUM_TYPE (
